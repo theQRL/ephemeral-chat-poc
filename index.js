6a88203c07590102e3290227ca103ad7aa6ae291
JP Lomas <jp.lomas@gmail.com>
2020-01-18 13:58:25 +0000

    Update deps location

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();
 var path = require('path');
 var server = require('http').createServer(app);
 
-var io = require('../..')(server);
+var io = require('socket.io')(server);
 
 var port = process.env.PORT || 3000;
 
